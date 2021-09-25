4c5ec1038ee22d026213843e3925dca7a84492b5
jamie-fernandez <jamie.fernandez.dev@gmail.com>
2021-09-25 16:54:46 -0500

    Accepts submit to update notifications

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,7 +1,8 @@
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 from .views import NotificationViewSet
 
-router = SimpleRouter()
-router.register('', NotificationViewSet)
+router = DefaultRouter()
+router.register(r'|notifications|notification|submit',
+                NotificationViewSet, basename='notifications')
 
 urlpatterns = router.urls
