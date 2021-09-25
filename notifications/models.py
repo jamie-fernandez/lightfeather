53f656603018d20ce6628ad6c0aa6427938c00c7
jamie-fernandez <jamie.fernandez.dev@gmail.com>
2021-09-25 12:43:34 -0500

    Updates phone and email to be optional fields in model

--- notifications/models.py
+++ notifications/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Notification(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    phone_number = models.CharField(max_length=10)
-    email = models.EmailField(max_length=320)
+    phone_number = models.CharField(blank=True, null=True, max_length=10)
+    email = models.EmailField(blank=True, null=True, max_length=320)
     supervisors = models.CharField(max_length=60)
 
     def __str__(self):
