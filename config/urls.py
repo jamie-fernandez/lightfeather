328adc1c98e6f331149f0d39dc676f241040953c
jamie-fernandez <jamie.fernandez.dev@gmail.com>
2021-09-25 16:40:15 -0500

    Updates notifications url

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('notifications/', include('notifications.urls')),
+    path('api/notifications/', include('notifications.urls')),
 ]
